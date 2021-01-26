fe563a346f85149c01ce1e207df43b0bb0fef690
Thiago Augusto <taugustoas@gmail.com>
2021-01-26 12:19:11 -0300

    get featured

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -3,11 +3,14 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+
 import './App.css'
 
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState();
 
     useEffect(() => {
 
@@ -15,6 +18,14 @@ export default () => {
             //Geting film's list
             const list = await Tmdb.getHomeList();
             setMovieList(list);
+
+            //Geting Featured
+            const originals = list.filter(i=>i.slug === 'originals');
+
+            const randomChosen = Math.floor(Math.random()*(originals[0].items.results.length - 1));
+            const chosen = originals[0].items.results[randomChosen];
+
+            console.log(chosen);
         }
 
         loadAll();
@@ -23,6 +34,11 @@ export default () => {
 
     return (
         <div className="page">
+
+            {featuredData &&
+                <FeaturedMovie item={featuredData}/>
+            }
+
             <section className="lists">
                 {movieList.map((item, key) => (
                     <MovieRow key={key} title={item.title} items={item.items}/>
