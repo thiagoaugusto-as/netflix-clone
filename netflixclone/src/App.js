f31b5cdbd542856f08948748aa33ddd67f77a210
Thiago Augusto <taugustoas@gmail.com>
2021-01-26 10:51:42 -0300

    iniciando component list row

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 
+import MovieRow from './components/MovieRow';
+
 export default () => {
+
+    const [movieList, setMovieList] = useState([]);
 
     useEffect(() => {
 
         const loadAll = async () => {
-            let list = await Tmdb.getHomeList();
-            console.log(list);
+            //Geting film's list
+            const list = await Tmdb.getHomeList();
+            setMovieList(list);
         }
 
         loadAll();
@@ -15,8 +20,12 @@ export default () => {
     }, []);
 
     return (
-        <div>
-            Olá Mundo!
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
         </div>
     );
 }
---
+++ netflixclone/src/components/MovieRow/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+export default ({title, items}) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+        </div>
+    );
+}
